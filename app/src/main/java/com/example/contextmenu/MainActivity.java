b882825723b47e1ae2e5653819363aa2a95cb382
shivaram0005 <shivaram8534@gmail.com>
2020-12-14 10:13:36 +0530

    ContextMenu in Android

---
+++ app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.contextmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerForContextMenu(b1);
+
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menumain, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "" + item.toString(), Toast.LENGTH_SHORT).show();
+            case R.id.item2:
+                Toast.makeText(this, "" + item.toString(), Toast.LENGTH_SHORT).show();
+            case R.id.item3:
+                Toast.makeText(this, "" + item.toString(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onContextItemSelected(item);
+    }
+}
